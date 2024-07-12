672e1b31b30677e7c95b22e290bd44c1590e622d
vaishu-shan <vaishnavishanmugam@usqaure.info>
2024-07-12 18:16:04 +0530

    Params issue fix.

--- controllers/token.controller.js
+++ controllers/token.controller.js
@@ -5,11 +5,16 @@ import asyncHandler from "express-async-handler"
 export const getPopularToken = asyncHandler( async(req,res)=>{
     try{
         const { chainId, include24HourPrice } = req.query;
-        let query = {};
-        if (chainId) {
-          query.chainId = chainId;
+        if (!chainId) {
+          return res.status(400).send({ error: 'chainId is required' });
         }
-        const tokens = await Token.find(query).select(include24HourPrice === 'true' ? 'chainId address decimals name symbol logo rank price last24hPrice' : 'chainId address decimals name symbol logo rank price');
+    
+        let projection = { __v: 0 }; // Exclude the __v field
+        if (include24HourPrice === 'false') {
+          projection.last24hPrice = 0; // Exclude last24hPrice if include24HourPrice is false
+        }
+    
+        const tokens = await Token.find({ chainId }, projection);
         res.status(200).send(tokens);
     }catch{
         res.status(500).json({ message: e })
